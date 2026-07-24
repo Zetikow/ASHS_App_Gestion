630cf1b41999a7498a2912cb44b19d2d897ec745
Zetikow <maximilien.mrch@gmail.com>
2026-07-24 15:20:51 +0200

    Configure l'URL de déploiement du Web App Apps Script

--- config/google-script-config.js
+++ config/google-script-config.js
@@ -9,4 +9,4 @@
 // À COMPLÉTER dès que le club fournit un compte Google : crée un nouveau Google Sheet, colle-y
 // le contenu de apps-script/ (voir apps-script/README.md), déploie en Application Web, puis
 // colle l'URL obtenue ici. En attendant, l'appli charge mais aucun appel serveur ne fonctionnera.
-const GOOGLE_SCRIPT_URL = "";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwIDGdLxWYznt6WrHF27TPlo6p7DwvLArJaYnSuldp9fhNf5VX310hJoHhwKOx5hF6qxw/exec";
